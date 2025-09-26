5d77874b19fb75a9d0d01171c48f3ef29ee4d748
Kyle Stephenson <kyle.stephenson@sling.com>
2025-09-26 08:09:20 -0600

    starting point

---
+++ cdk-examples/oracle-rds/test/oracle-rds.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as OracleRds from '../lib/oracle-rds-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/oracle-rds-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new OracleRds.OracleRdsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
